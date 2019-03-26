1d89fd6585b90cf5b33d90b60d16e6580633ee65
fweb.cc <48904755+fwebcc@users.noreply.github.com>
2019-03-26 09:28:07 +0800

    Update wifi.py

--- wifi.py
+++ wifi.py
@@ -8,20 +8,24 @@ setting = json.load(f)
 #s = wlan.config('mac')
 #mymac = ('%02x-%02x-%02x-%02x-%02x-%02x') %(s[0],s[1],s[2],s[3],s[4],s[5])
 #开启WIFI链接
-def GO_wifi():
-       wlan = network.WLAN(network.STA_IF)
-       if not wlan.isconnected():
-          wlan.active(False)
+wlan = network.WLAN(network.STA_IF)
+def do_connect():
           wlan.active(True)
           wlan.config(dhcp_hostname=setting['MQTT_Topic'])
           wlan.connect(setting['ESSID'], setting['PASSWORD'])
-          print('链接失败无网络', wlan.ifconfig())
+          print('链接失败无网络', wlan.ifconfig(),wlan.config('dhcp_hostname'))
+def GO_wifi():
+       if not wlan.isconnected():
+          do_connect()
        else:
-          print('链接成功', wlan.ifconfig())
+          if setting['MQTT_Topic'] ==  wlan.config('dhcp_hostname'):
+              print('链接成功', wlan.ifconfig(),wlan.config('dhcp_hostname'))
+          else:
+              wlan.active(False)
+              do_connect()
 
 #判断网络链接没开启开启AP
 def AP_START():
-        wlan = network.WLAN(network.STA_IF)
         wlan.active(False)
         ap_if = network.WLAN(network.AP_IF)
         ap_if.active(True)
